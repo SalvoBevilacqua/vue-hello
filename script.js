b8b2e5536072c713b172dc72b07bd89f956deb25
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-13 16:02:15 +0100

    fix:class

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const app = createApp({
   data() {
     return {
       stringMessage: "Saaaalve!",
-      text_alignment: "text-center",
-      img_alignment: "rounded mx-auto d-block",
+      text_class: "text-center m-5",
+      img_class: "rounded mx-auto d-block",
       imgUrl: "https://hips.hearstapps.com/hmg-prod/images/ber-1-1556271580.jpg"
     };
   }
