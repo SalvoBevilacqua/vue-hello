163b4f5b9d0db83c965c94987a3916e822157959
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-13 16:03:05 +0100

    fix:title

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      stringMessage: "Saaaalve!",
+      stringMessage: "Berserk",
       text_class: "text-center m-5",
       img_class: "rounded mx-auto d-block",
       imgUrl: "https://hips.hearstapps.com/hmg-prod/images/ber-1-1556271580.jpg"
